25f2b7fb0664f322a55ec2440d66de8255baf2be
Eduardo Garcia <Edrogarcia08@outlook.com>
2022-05-04 22:46:22 -0600

    InicioSesion

--- app/src/main/java/com/purosurf/minibar/Vista/SplashActivity_Vista.java
+++ app/src/main/java/com/purosurf/minibar/Vista/SplashActivity_Vista.java
@@ -2,17 +2,18 @@ package com.purosurf.minibar.Vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import com.purosurf.minibar.Vista.InicioSesion.IniciarSesion;
 
-import com.purosurf.minibar.R;
-
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity_Vista extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent Login_Vista  = new Intent(this , Login_Vista.class);
+        Intent Login_Vista  = new Intent(this , IniciarSesion.class);
         startActivity(Login_Vista );
         finish();
     }
