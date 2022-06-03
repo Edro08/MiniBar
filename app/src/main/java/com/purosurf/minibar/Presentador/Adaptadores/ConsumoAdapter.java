1fc006c34900330f264b079130845c71850ff61d
gda1194 <98855649+gda1194@users.noreply.github.com>
2022-06-03 11:31:53 -0600

    Ajustes minuciosos

--- app/src/main/java/com/purosurf/minibar/Presentador/Adaptadores/ConsumoAdapter.java
+++ app/src/main/java/com/purosurf/minibar/Presentador/Adaptadores/ConsumoAdapter.java
@@ -37,7 +37,7 @@ public class ConsumoAdapter extends RecyclerView.Adapter<ConsumoAdapter.ViewHold
     // Seleccionar el contenedor CardView XML
     @Override
     public ConsumoAdapter.ViewHolder onCreateViewHolder (ViewGroup parent, int viewType){
-        View view = minFlater.inflate(R.layout.cardview_lista_recyclerview, null );
+        View view = minFlater.inflate(R.layout.cardview_lista_recyclerview, parent, false );
 
         //declarar onclick
         view.setOnClickListener(this);
